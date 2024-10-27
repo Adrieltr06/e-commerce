6940ee506b5bc0113413b2b6a08d4e8718e09370
Muyu <muyumuyu123@gmail.com>
2024-10-27 20:06:22 +0700

    Ariandi

--- src/main/kotlin/id/co/bca/intra/e_commerce/model/Order.kt
+++ src/main/kotlin/id/co/bca/intra/e_commerce/model/Order.kt
@@ -1,12 +1,20 @@
 package id.co.bca.intra.e_commerce.model
 
-import jakarta.persistence.*
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.ManyToOne
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 
-@Entity
+//import jakarta.persistence.*
+
+//@Entity
+@Table(value = "orders")
 data class Order(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
+    @Id val id: Long,
+
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     @ManyToOne
     @JoinColumn(name = "account_id")
--- src/main/kotlin/id/co/bca/intra/e_commerce/model/Product.kt
+++ src/main/kotlin/id/co/bca/intra/e_commerce/model/Product.kt
@@ -1,12 +1,17 @@
 package id.co.bca.intra.e_commerce.model
 
-import jakarta.persistence.*
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
 
-@Entity
+//import jakarta.persistence.*
+
+//@Entity
+
+@Table(value = "products")
 data class Product(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long,
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id val id: Long,
     var name: String,
     var price: Double,
     var description: String
