12c8f3d9844278dc93f480645ca26b5f8e46eca8
choirunnisaptr <49263740+choirunnisaptr@users.noreply.github.com>
2024-10-29 14:49:10 +0700

    Update CRUD Product Controller

--- src/main/kotlin/id/co/bca/intra/e_commerce/controller/ProductController.kt
+++ src/main/kotlin/id/co/bca/intra/e_commerce/controller/ProductController.kt
@@ -17,4 +17,25 @@ class ProductController(private val productService: ProductService) {
     suspend fun searchProduct(@PathVariable name: String): List<Product> {
         return productService.searchProduct(name)
     }
+
+    @PutMapping("/{id}/name/{name}")
+    suspend fun updateProductName(@PathVariable id: Long, @PathVariable name: String): Mono<Product> =
+        productService.updateProductName(id, name).switchIfEmpty(
+            Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND)))
+
+    @PutMapping("/{id}/price/{address}")
+    suspend fun updateProductPrice(@PathVariable id: Long, @PathVariable price: Double): Mono<Product> =
+        productService.updateProductPrice(id, price).switchIfEmpty(
+            Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND)))
+
+    @PutMapping("/{id}/description/{address}")
+    suspend fun updateProductDescription(@PathVariable id: Long, @PathVariable description: String): Mono<Product> =
+        productService.updateProductDescription(id, description).switchIfEmpty(
+            Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND)))
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    suspend fun deleteProduct(@PathVariable id: Long) : Mono<Void> =
+        productService.deleteProduct(id).switchIfEmpty(
+            Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND)))
 }
