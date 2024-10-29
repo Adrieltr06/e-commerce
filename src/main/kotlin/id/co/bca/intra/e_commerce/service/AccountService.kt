bd0de51a6ccc172c06922141806dd45d58298c22
choirunnisaptr <49263740+choirunnisaptr@users.noreply.github.com>
2024-10-29 14:51:52 +0700

    Update CRUD Account Service

--- src/main/kotlin/id/co/bca/intra/e_commerce/service/AccountService.kt
+++ src/main/kotlin/id/co/bca/intra/e_commerce/service/AccountService.kt
@@ -19,4 +19,22 @@ class AccountService(private val accountRepository: AccountRepository) {
     suspend fun createUser(account: Account): Account {
         return accountRepository.save(account).awaitFirstOrNull() ?: throw Exception("Failed to create user")
     }
+
+    suspend fun updateUserName(id: Long, name: String): Mono<Account> {
+        return accountRepository.findById(id)
+            .flatMap {
+                val updatedAccount = it.copy(name = name, address = it.address, balance = it.balance, isAdmin = it.isAdmin)
+                accountRepository.save(updatedAccount)
+            }
+    }
+
+    suspend fun updateUserAddress(id: Long, address: String): Mono<Account> {
+        return accountRepository.findById(id)
+            .flatMap {
+                val updatedAccount = it.copy(name = it.name, address = address, balance = it.balance, isAdmin = it.isAdmin)
+                accountRepository.save(updatedAccount)
+            }
+    }
+
+    suspend fun deleteUser(id: Long): Mono<Void> = accountRepository.deleteById(id)
 }
